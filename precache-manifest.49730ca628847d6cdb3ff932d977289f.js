b059e423f9e005b4f011d19ad80ee9d0b1e42f8a
Sharma10 <PSharma10@ad.harman.com>
2020-08-02 21:56:35 +0530

    Updates

---
+++ precache-manifest.49730ca628847d6cdb3ff932d977289f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7911b70c80c83d11eb725c4b40b70dc",
+    "url": "/deployreactJiraApp/index.html"
+  },
+  {
+    "revision": "368e3e1dd5e3f78f5813",
+    "url": "/deployreactJiraApp/static/css/2.daf4b330.chunk.css"
+  },
+  {
+    "revision": "a28250d41baaade909de",
+    "url": "/deployreactJiraApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "368e3e1dd5e3f78f5813",
+    "url": "/deployreactJiraApp/static/js/2.b0ad6db9.chunk.js"
+  },
+  {
+    "revision": "eb85fa2ff650be4fb535d1bc70e9f226",
+    "url": "/deployreactJiraApp/static/js/2.b0ad6db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28250d41baaade909de",
+    "url": "/deployreactJiraApp/static/js/main.37cc60bb.chunk.js"
+  },
+  {
+    "revision": "6de9be7b5711460782f4",
+    "url": "/deployreactJiraApp/static/js/runtime-main.87b2b594.js"
+  }
+]);
